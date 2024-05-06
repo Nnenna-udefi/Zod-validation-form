90e208bd346c068b87157e5a8341594aba42d51a
Nnenna-udefi <miriamudefi@gmail.com>
2024-05-06 22:38:05 +0100

    chore: got more createive with zod

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { ZodForm } from "./components/zodform";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-row gap-10 items-center justify-between p-24">
+    <main className="flex min-h-screen md:flex-row flex-col md:gap-10 gap-0 items-center justify-between p-24">
       <ZodForm />
       <Form />
     </main>
